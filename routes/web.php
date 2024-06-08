7f3611a79d7c121b1160b3b46e7008107ee2887b
Paul Flores <paulfloresdev@icloud.com>
2024-06-07 21:21:51 -0600

    Add Post rout for CRUD with show exception

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 
 
 Route::controller(PageController::class)->group(function () {
@@ -21,5 +22,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('posts', PostController::class)->except(['show']);
 
 require __DIR__.'/auth.php';
